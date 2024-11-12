df7436b3902ee960889e0d169f5441f29112b8f4
tiennguyen <nguyenthanhtien.kr0407@gmail.com>
2024-11-12 17:06:08 +0700

    fix: remove tag logic

--- ssh-connect.sh
+++ ssh-connect.sh
@@ -18,16 +18,19 @@ update_history() {
 prepend_recent_selections() {
     if [ -s "$HISTORY_FILE" ]; then
         echo "Recent - Top 5 commands"
-        cat "$HISTORY_FILE" | sort -u | tail -n 5 | tac | awk '{print NR ": " $0}'
+        # remove duplicate lines and empty lines, then sort and reverse the list
+        cat "$HISTORY_FILE" | sort -u | tail -n 5 | tac | grep '.' | awk '{print NR ": " $0}'
     fi
 }
 
-# Step 1: Choose organization with preview of its tags
+# Step 1: Choose organization with preview of its name
 org_selection=$(prepend_recent_selections; jq -r '.[].org' "$COMMANDS_FILE" | sort -u | awk '{print $0}')
-org=$(echo "$org_selection" | fzf --height=10 --header="Select Organization or Recent" --preview='export org={} && if [[ "$org" =~ "Top 5" ]]; then tail -n 5 '"$HISTORY_FILE"' | sort -u | tac; elif [[ "$org" =~ "ssh" ]]; then echo $org; else jq -r --arg org $org ".[] | select(.org==\$org) | .tag" '"$COMMANDS_FILE"' | sort -u; fi')
+org=$(echo "$org_selection" | fzf --height=10 --header="Select Organization or Recent" --preview='export org={} && if [[ "$org" =~ "Top 5" ]]; then tail -n 5 '"$HISTORY_FILE"' | sort -u | tac; elif [[ "$org" =~ "ssh" ]]; then echo $org; else jq -r --arg org $org ".[] | select(.org==\$org) | .name" '"$COMMANDS_FILE"' | sort -u; fi')
 
 if [[ "$org" =~ "ssh" ]]; then
+
     command=$(echo "$org" | cut -d'|' -f3)
+
     echo "Executing command: $command"
     save_history="${org:3}"
     update_history "$save_history"
@@ -40,6 +43,7 @@ fi
 # If "Recent" was selected, skip to executing a recent command
 if [[ "$org" == "Recent - Top 5 commands" ]]; then
     selected_command=$(tail -n 5 "$HISTORY_FILE" | tac | fzf --header="Select a recent command" --preview="echo {} | cut -d'|' -f3")
+    echo "$selected_command"
     command=$(echo "$selected_command" | cut -d'|' -f3)
 
     update_history "$selected_command"
@@ -54,17 +58,12 @@ if [[ "$org" == "Recent - Top 5 commands" ]]; then
     fi
 fi
 
-# Step 2: Choose tag based on organization with preview of its commands
-tag=$(jq -r --arg org "$org" '.[] | select(.org==$org) | .tag' "$COMMANDS_FILE" | sort -u | fzf --height=10 --header="Select Tag" --preview="export tag={} && echo \$tag | jq -r --arg tag \$tag '.[] | select(.org==\"$org\" and .tag==\$tag) | .name' $COMMANDS_FILE")
-[ -z "$tag" ] && exit 1
-
-# Step 3: Choose name based on tag
-name_cmd=$(jq -r --arg org "$org" --arg tag "$tag" '.[] | select(.org==$org and .tag==$tag) | "\(.name)"' "$COMMANDS_FILE" | fzf --height=15 --header="Select Name" --preview="export name={} && echo \$name | jq -r --arg name \$name '.[] | select(.org==\"$org\" and .tag==\"$tag\" and .name==\$name) | .command' $COMMANDS_FILE")
-
+# Step 2: Choose name based on organization with preview of its commands
+name_cmd=$(jq -r --arg org "$org" '.[] | select(.org==$org) | .name' "$COMMANDS_FILE" | sort -u | fzf --height=10 --header="Select Name" --preview="export name={} && echo \$name | jq -r --arg name \$name '.[] | select(.org==\"$org\" and .name==\$name) | .command' $COMMANDS_FILE")
 [ -z "$name_cmd" ] && exit 1
 
 # Step 4: Find command and execute
-command=$(jq -r --arg org "$org" --arg tag "$tag" --arg name "$name_cmd" '.[] | select(.org==$org and .tag==$tag and .name==$name) | .command' "$COMMANDS_FILE")
+command=$(jq -r --arg org "$org" --arg name "$name_cmd" '.[] | select(.org==$org and .name==$name) | .command' "$COMMANDS_FILE")
 
 if [[ -n "$command" ]]; then
     echo "Executing command: $command"
